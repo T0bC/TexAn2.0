5842f55cc5ffcebdce3df2d73ffd97cec50c10f0
T0bC <59888795+T0bC@users.noreply.github.com>
2026-02-12 09:54:26 +0100

    feat: integrate three-way robust ANOVA (t3way) into statistics module with bootstrap support and update error message for unsupported multi-way tests

--- app/view/statistics/statistics.R
+++ app/view/statistics/statistics.R
@@ -302,12 +302,21 @@ server <- function(id, input_data, data_version,
               boot_samples = params$boot_samples,
               boot_sample_size = params$boot_sample_size
             )
+          } else if (n_ways == 3) {
+            robust_tests$perform_t3way(
+              df = data,
+              x_axis = x_cols,
+              measure_col = m,
+              tr_value = tr_val,
+              use_bootstrap = params$use_bootstrap,
+              boot_samples = params$boot_samples,
+              boot_sample_size = params$boot_sample_size
+            )
           } else {
-            # t3way not yet implemented
             error_handling$simple_error(
               message = paste0(
                 n_ways,
-                "-way robust test not yet implemented."
+                "-way robust test is not supported."
               ),
               operation_name = "statistics_compute"
             )
