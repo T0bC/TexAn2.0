501948feb0a51de8a7c24f1c0e973e9576ac785a
T0bC <59888795+T0bC@users.noreply.github.com>
2026-02-13 12:58:30 +0100

    feat: export calculate_optimal_components function from PCA logic module

--- app/logic/pca/__init__.R
+++ app/logic/pca/__init__.R
@@ -2,6 +2,7 @@
 box::use(
   app/logic/pca/kmo[calculate_kmo],
   app/logic/pca/na_handling[clean_na_rows],
+  app/logic/pca/optimal_components[calculate_optimal_components],
   app/logic/pca/pca[validate_inputs, run_analysis],
   app/logic/pca/scaling[scale_data],
 )
