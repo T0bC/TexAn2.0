34a5d61531d5215c82bb484ea3e3363ce587c30c
T0bC <59888795+T0bC@users.noreply.github.com>
2026-02-13 13:21:52 +0100

    refactor: rename run_analysis to run_pca and add create_pca_excel export function to PCA module

--- app/logic/pca/__init__.R
+++ app/logic/pca/__init__.R
@@ -3,6 +3,7 @@ box::use(
   app/logic/pca/kmo[calculate_kmo],
   app/logic/pca/na_handling[clean_na_rows],
   app/logic/pca/optimal_components[calculate_optimal_components],
-  app/logic/pca/pca[validate_inputs, run_analysis],
+  app/logic/pca/pca[validate_inputs, run_pca],
+  app/logic/pca/pca_export[create_pca_excel],
   app/logic/pca/scaling[scale_data],
 )
