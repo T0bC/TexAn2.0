18aec704cf927feee3bbb91c4f59309e71e376f9
T0bC <59888795+T0bC@users.noreply.github.com>
2026-02-13 12:35:20 +0100

    feat: export scale_data function from PCA logic module

--- app/logic/pca/__init__.R
+++ app/logic/pca/__init__.R
@@ -3,4 +3,5 @@ box::use(
   app/logic/pca/kmo[calculate_kmo],
   app/logic/pca/na_handling[clean_na_rows],
   app/logic/pca/pca[validate_inputs, run_analysis],
+  app/logic/pca/scaling[scale_data],
 )
