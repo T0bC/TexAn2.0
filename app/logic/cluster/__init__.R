a16693d7048c8334d2ccbc8b3f3921c3b6b49e10
T0bC <59888795+T0bC@users.noreply.github.com>
2026-02-17 14:07:30 +0100

    refactor: replace distance_matrix import with hopkins module in cluster logic exports

--- app/logic/cluster/__init__.R
+++ app/logic/cluster/__init__.R
@@ -1,5 +1,5 @@
 #' @export
 box::use(
   app/logic/cluster/cluster[validate_inputs, run_clustering],
-  app/logic/cluster/distance_matrix[compute_distance_matrix],
+  app/logic/cluster/hopkins[compute_hopkins],
 )
