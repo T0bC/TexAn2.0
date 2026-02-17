f02c13ca6678a54a8b78a7312cbfd9d0cab6ce7c
T0bC <59888795+T0bC@users.noreply.github.com>
2026-02-17 14:20:30 +0100

    refactor: add optimal_clusters module import to cluster logic exports

--- app/logic/cluster/__init__.R
+++ app/logic/cluster/__init__.R
@@ -2,4 +2,5 @@
 box::use(
   app/logic/cluster/cluster[validate_inputs, run_clustering],
   app/logic/cluster/hopkins[compute_hopkins],
+  app/logic/cluster/optimal_clusters[compute_optimal_clusters],
 )
