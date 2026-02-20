ed8f89f275dbec9262bcdbab522b2993d0e23510
T0bC <59888795+T0bC@users.noreply.github.com>
2026-02-20 12:22:02 +0100

    feat: add data splitting and prediction exports to LDA module initialization

--- app/logic/lda/__init__.R
+++ app/logic/lda/__init__.R
@@ -1,8 +1,12 @@
 #' @export
 box::use(
+  app/logic/lda/data_splitting[
+    create_stratified_split,
+  ],
   app/logic/lda/lda[
     lda_error_parser,
     run_lda,
+    run_predict,
     run_qda,
     validate_inputs,
   ],
