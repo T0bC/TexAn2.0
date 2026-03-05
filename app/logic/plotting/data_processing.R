d493f5f19231890cc59dab007baa882ada46cde2
T0bC <59888795+T0bC@users.noreply.github.com>
2026-03-05 11:51:27 +0100

    refactor: update normalize module import path from plotting to preprocessing directory

--- app/logic/plotting/data_processing.R
+++ app/logic/plotting/data_processing.R
@@ -5,7 +5,7 @@ box::use(
 box::use(
   app/logic/data_utils,
   app/logic/plotting/assumption_checks,
-  app/logic/plotting/normalize,
+  app/logic/preprocessing/normalize,
 )
 
 # =============================================================================
